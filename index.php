386384d9e69e8f8d55a3b5f6159d6e59be983c02
dgagul <dario.gagulic@uzh.ch>
2019-10-06 19:07:21 +0200

    changed script source https instead of http

--- index.php
+++ index.php
@@ -225,7 +225,7 @@
       </div>
     </footer>
 
-    <script src="http://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/mobile-menu.js"></script>
     <script type="text/javascript" src="js/alert.js"></script>
 
