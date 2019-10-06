aa4dbe8435074d6e62b21c2f870f8416beffd72f
dgagul <dario.gagulic@uzh.ch>
2019-10-06 22:14:23 +0200

    changed FB link

--- index.php
+++ index.php
@@ -212,7 +212,7 @@
       <div class="inhalt">
         <div class="follow">
           <p>Follow us</p>
-          <a href="https://www.facebook.com/campaign/landing.php?&campaign_id=1557999560&extra_1=s%7Cc%7C294722991322%7Ce%7Cfacebook%7C&placement=&creative=294722991322&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D1557999560%26adgroupid%3D61885662631%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D1t1%26target%3D%26targetid%3Dkwd-541132862%26loc_physical_ms%3D1003054%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=EAIaIQobChMIqtaHivXy4wIVBUQYCh0BEgoKEAAYASAAEgJPcPD_BwE" target="_blank">
+          <a href="https://www.facebook.com/sutis.imbiss.7" target="_blank">
             <i class="fab fa-facebook-square"></i></a>
           <a href="#"><i class="fab fa-instagram"></i></a>
         </div>
